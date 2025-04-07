bb087ee0cf6af9849ac33e6c85f2023f36aaefed
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2025-04-07 16:17:04 +0700

    Feat: Navigation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Page from "./components/section/Page.jsx";
+import Header from "./components/section/Header.jsx";
+import Navigation from "./components/section/Navigation.jsx";
+
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <Page>
+      <Header>
+        <Navigation />
+      </Header>
+    </Page>
+  );
 }
 
 export default App;
---
+++ src/components/section/Header.jsx
@@ -0,0 +1,8 @@
+const Header = ({children}) => {
+  return (
+    <div className="bg-gradient-to-bottom py-10">
+      {children}
+    </div>
+  );
+};
+export default Header;
